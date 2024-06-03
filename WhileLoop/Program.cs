a8d536126be434af01e13bed02d3b8a2a1269d77
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-06-03 22:46:27 +0600

    file added

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("This is from for loop");
+
+            int num = 1;
+
+            while(num !=11)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+            Console.WriteLine("This is from while loop");
+
+            int number = 1;
+
+            do 
+            {
+                Console.WriteLine(number);
+                number++;
+
+
+            } while(number<11);
+            Console.WriteLine("This is from do while loop");
+        }
+    }
+}
